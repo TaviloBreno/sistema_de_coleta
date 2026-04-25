5f0e317aba274dc6b4325fab8f227d3e23a88715
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-25 19:45:42 -0300

    feat: implement app layout with navigation, sidebar, and routes for map and analytics views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+import { useAuthStore } from '@/stores/useAuthStore'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <div class="min-vh-100 bg-body-tertiary">
     <Navbar />
-    <main>
+
+    <div v-if="authStore.isAuthenticated" class="container-fluid">
+      <div class="row">
+        <div class="d-none d-md-block col-md-3 col-lg-2 px-0">
+          <Sidebar />
+        </div>
+        <main class="col-12 col-md-9 col-lg-10">
+          <RouterView />
+        </main>
+      </div>
+    </div>
+
+    <main v-else>
       <RouterView />
     </main>
   </div>
